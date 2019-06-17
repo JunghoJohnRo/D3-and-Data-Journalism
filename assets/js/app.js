cafc842951a029c1002e3d7e9634a6a40c6196b8
JunghoJohnRo <junghojohnro@gmail.com>
2019-06-17 12:57:23 -0500

    Initial Version

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import data from the CSV file
+var csvFile = "/data/data.csv";
+d3.csv(csvFile, function(error, newsData) {
+  if (error) throw error;
+
+  console.log(newsData);
+
+
+
+})
